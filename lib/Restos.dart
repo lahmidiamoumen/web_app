b7565718cbd2c88e2170a3a8b6adda8cf0e006e1
lahmidiamoumen <a.lahmidi@univ-boumerdes.dz>
2020-12-24 09:09:58 +0100

    change urls to weach view

---
+++ lib/Restos.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class Restos extends StatefulWidget {
+  final String title;
+  final String url;
+  Restos({this.url, this.title});
+
+  @override
+  State<StatefulWidget> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<Restos>
+    with SingleTickerProviderStateMixin {
+  InAppWebViewController webView;
+  String url = "https://allrestos.com/?search_term=&lcats";
+  String titles = "Restos - allrestos";
+  double progress = 0;
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      new GlobalKey<RefreshIndicatorState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(this.titles),
+        bottom: PreferredSize(
+          preferredSize: Size(double.infinity, 1.0),
+          child: progress < 1.0
+              ? LinearProgressIndicator(
+                  value: progress,
+                  backgroundColor: Colors.blue,
+                )
+              : Container(),
+        ),
+      ),
+      body: myBody(),
+    );
+  }
+
+  Future<Null> _refresh() async {
+    return await webView.reload();
+  }
+
+  Widget myBody() {
+    return RefreshIndicator(
+      key: _refreshIndicatorKey,
+      onRefresh: _refresh,
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                child: InAppWebView(
+                  initialUrl: this.url,
+                  initialHeaders: {},
+                  initialOptions: InAppWebViewGroupOptions(
+                      crossPlatform: InAppWebViewOptions(
+                    debuggingEnabled: false,
+                  )),
+                  onWebViewCreated: (InAppWebViewController controller) {
+                    webView = controller;
+                  },
+                  onLoadStart:
+                      (InAppWebViewController controller, String url) async {
+                    await webView.evaluateJavascript(
+                        source:
+                            """document.getElementsByClassName('main-header')[0].style.display = 'none';
+                                 document.getElementsByClassName('main-footer')[0].style.display = 'none';
+                              """);
+                    setState(() {
+                      this.url = url;
+                    });
+                  },
+                  onLoadStop:
+                      (InAppWebViewController controller, String url) async {
+                    await webView.evaluateJavascript(
+                        source: """document.getElementById('wrapper').remove();
+                               document.getElementsByClassName('main-header')[0].style.display = 'none';
+                               document.getElementsByClassName('main-footer')[0].style.display = 'none';
+                              """);
+                    String tt = await controller.getTitle();
+                    setState(() {
+                      this.titles = tt;
+                      this.url = url;
+                    });
+                  },
+                  onProgressChanged:
+                      (InAppWebViewController controller, int progress) {
+                    setState(() {
+                      this.progress = progress / 100;
+                    });
+                  },
+                ),
+              ),
+            ),
+            /* ButtonBar(
+                alignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    child: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      if (webView != null) {
+                        webView.goBack();
+                      }
+                    },
+                  ),
+                  RaisedButton(
+                    child: Icon(Icons.arrow_forward),
+                    onPressed: () {
+                      if (webView != null) {
+                        webView.goForward();
+                      }
+                    },
+                  ),
+                  RaisedButton(
+                    child: Icon(Icons.refresh),
+                    onPressed: () {
+                      if (webView != null) {
+                        webView.reload();
+                      }
+                    },
+                  ),
+                ],
+              ) */
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:webview_flutter/webview_flutter.dart' as ds;
-
+import 'Home.dart';
+import 'Maps.dart';
+import 'Restos.dart';
 import 'custom_icons_icons.dart';
 
 Future main() async {
@@ -16,39 +17,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(url: "https://allrestos.com/"),
-      title: 'allrestos',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      initialRoute: '/main',
+      routes: {
+        '/main': (context) => MyHomePage(),
+        '/home': (context) => Home(
+            url: "https://allrestos.com/", title: "Restos Map - allrestos"),
+        '/map': (context) => Maps(
+            url: "https://allrestos.com/index.php/restos-map/",
+            title: "Accueil | allrestos"),
+        '/restos': (context) => Restos(
+            url: "https://allrestos.com/?search_term=&lcats",
+            title: "Restos - allrestos"),
+      },
+      theme: ThemeData(primarySwatch: Colors.blue),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({this.url});
-  final String url;
-
   @override
-  _MyWebView createState() => _MyWebView(url: this.url);
+  _MyWebView createState() => _MyWebView();
 }
 
 class _MyWebView extends State<MyHomePage> {
   List<GlobalKey> _key = [GlobalKey(), GlobalKey(), GlobalKey()];
   List<double> pos = [60, 60, 60];
   int _currentIndex = 1;
-  InAppWebViewController webView;
-  _MyWebView({this.url});
-  String url;
-  String titles = 'Accueil | allrestos';
-  double progress = 0;
-
-  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
-      new GlobalKey<RefreshIndicatorState>();
-
-  Future<Null> _refresh() async {
-    return await webView.reload();
-  }
+  List<Widget> _children;
 
   void _getSizes() {
     for (int i = 0; i < _key.length; i++) {
@@ -57,34 +52,26 @@ class _MyWebView extends State<MyHomePage> {
     }
   }
 
+  void _onTab(int value) {
+    setState(() {
+      _currentIndex = value;
+    });
+  }
+
   void _afterLayout(_) {
     //_onTab(1);
     _getSizes();
-  }
-
-  Future<Null> get home async {
-    Navigator.pop(context);
-    setState(() {
-      this.url = "https://livesawa.com/";
-      this.titles = 'Accueil | LiveSawa';
-    });
-    return await webView.loadUrl(url: "https://livesawa.com/");
-  }
-
-
-  Future<Null> get retos async {
-    Navigator.pop(context);
-    setState(() {
-      this.url = "https://allrestos.com/?search_term=&lcats";
-      this.titles = 'Restos';
-    });
-    return await webView.loadUrl(url: "https://allrestos.com/?search_term=&lcats");
   }
 
   @override
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
     if (Platform.isAndroid) ds.WebView.platform = ds.SurfaceAndroidWebView();
+    _children = [
+      Maps(),
+      Home(),
+      Restos(),
+    ];
     super.initState();
   }
 
@@ -97,108 +84,8 @@ class _MyWebView extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text(this.titles),
-          bottom: PreferredSize(
-            preferredSize: Size(double.infinity, 1.0),
-            child: progress < 1.0
-                ? LinearProgressIndicator(
-                    value: progress,
-                    backgroundColor: Colors.yellow,
-                  )
-                : Container(),
-          ),
-        ),
-        body: myBody(),
+        body: _children[_currentIndex],
         bottomNavigationBar: bottomNavigationBar());
-  }
-
-
-  Widget myBody() {
-    return RefreshIndicator(
-      key: _refreshIndicatorKey,
-      onRefresh: _refresh,
-      child: Container(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                child: InAppWebView(
-                  initialUrl: this.url,
-                  initialHeaders: {},
-                  initialOptions: InAppWebViewGroupOptions(
-                      crossPlatform: InAppWebViewOptions(
-                    debuggingEnabled: false,
-                  )),
-                  onWebViewCreated: (InAppWebViewController controller) {
-                    webView = controller;
-                  },
-                  onLoadStart:
-                      (InAppWebViewController controller, String url) async {
-                    await webView.evaluateJavascript(
-                        source:
-                            """document.getElementsByClassName('main-header')[0].style.display = 'none';
-                                 document.getElementsByClassName('main-footer')[0].style.display = 'none';
-                              """);
-                    setState(() {
-                      this.url = url;
-                    });
-                  },
-                  onLoadStop:
-                      (InAppWebViewController controller, String url) async {
-                    await webView.evaluateJavascript(
-                        source:
-                            """document.getElementsByClassName('main-header')[0].style.display = 'none';
-                                 document.getElementsByClassName('main-footer')[0].style.display = 'none';
-                              """);
-                    String tt = await controller.getTitle();
-                    setState(() {
-                      this.titles = tt;
-                      this.url = url;
-                    });
-                  },
-                  onProgressChanged:
-                      (InAppWebViewController controller, int progress) {
-                    setState(() {
-                      this.progress = progress / 100;
-                    });
-                  },
-                ),
-              ),
-            ),
-            /* ButtonBar(
-                alignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  RaisedButton(
-                    child: Icon(Icons.arrow_back),
-                    onPressed: () {
-                      if (webView != null) {
-                        webView.goBack();
-                      }
-                    },
-                  ),
-                  RaisedButton(
-                    child: Icon(Icons.arrow_forward),
-                    onPressed: () {
-                      if (webView != null) {
-                        webView.goForward();
-                      }
-                    },
-                  ),
-                  RaisedButton(
-                    child: Icon(Icons.refresh),
-                    onPressed: () {
-                      if (webView != null) {
-                        webView.reload();
-                      }
-                    },
-                  ),
-                ],
-              ) */
-          ],
-        ),
-      ),
-    );
   }
 
   Widget bottomNavigationBar() {
@@ -207,43 +94,24 @@ class _MyWebView extends State<MyHomePage> {
         child: Stack(
           children: <Widget>[
             BottomNavigationBar(
-              showUnselectedLabels: false,
-              showSelectedLabels: false,
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              selectedIconTheme: IconThemeData(color: Colors.black, size: 26),
-              currentIndex: _currentIndex,
-              items: <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                    icon: Icon(CustomIcons.compass_1, key: _key[0]),
-                    label: "Map"),
-                BottomNavigationBarItem(
-                    icon: Icon(CustomIcons.home_1, key: _key[1]),
-                    label: "Home"),
-                BottomNavigationBarItem(
-                    icon: Icon(CustomIcons.chat, key: _key[2]),
-                    label: "Resto"),
-              ],
-              onTap: (value) async {
-                Navigator.pop(context);
-                switch (value) {
-                    case 0:
-                      await webView.loadUrl(url: "https://allrestos.com/index.php/restos-map/");
-                      setState(() {
-                        this.url = "https://allrestos.com/index.php/restos-map/";
-                        this.titles = 'Restos Map';
-                      });
-                      break;
-                    case 1:
-                      await home;
-                      break;
-                    case 2:
-                      await retos;
-                      break;
-                    default:
-                  }
-                }
-            ),
+                showUnselectedLabels: false,
+                showSelectedLabels: false,
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                selectedIconTheme: IconThemeData(color: Colors.black, size: 26),
+                currentIndex: _currentIndex,
+                items: <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                      icon: Icon(CustomIcons.compass_1, key: _key[0]),
+                      label: "Map"),
+                  BottomNavigationBarItem(
+                      icon: Icon(CustomIcons.home_1, key: _key[1]),
+                      label: "Home"),
+                  BottomNavigationBarItem(
+                      icon: Icon(CustomIcons.chat, key: _key[2]),
+                      label: "Resto"),
+                ],
+                onTap: _onTab),
             AnimatedPositioned(
               duration: Duration(microseconds: 300),
               left: pos[_currentIndex],
@@ -262,15 +130,20 @@ class _MyWebView extends State<MyHomePage> {
   }
 }
 
-class _GridTitleText extends StatelessWidget {
+class _GridTitleText extends StatefulWidget {
   const _GridTitleText(this.text);
 
   final String text;
 
   @override
+  __GridTitleTextState createState() => __GridTitleTextState();
+}
+
+class __GridTitleTextState extends State<_GridTitleText> {
+  @override
   Widget build(BuildContext context) {
     return FlexibleSpaceBar(
-      title: Text(text,
+      title: Text(widget.text,
           textAlign: TextAlign.start,
           style: TextStyle(color: Colors.black87, fontSize: 22.0)),
     );
